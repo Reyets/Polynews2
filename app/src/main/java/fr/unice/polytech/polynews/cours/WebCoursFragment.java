421b2ee564ae62d6bb202264d35adf6215b3067b
Thomas Jalabert <thomas.jalabert@gmail.com>
2016-04-18 10:52:32 +0200

    ajout de la vue

---
+++ app/src/main/java/fr/unice/polytech/polynews/cours/WebCoursFragment.java
@@ -0,0 +1,51 @@
+package fr.unice.polytech.polynews.cours;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+import fr.unice.polytech.polynews.R;
+
+/**
+ * Created by chronos on 4/1/16.
+ */
+public class WebCoursFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public WebCoursFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static WebCoursFragment newInstance(int sectionNumber) {
+        WebCoursFragment fragment = new WebCoursFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_cours_list, container, false);
+        WebView web = (WebView) rootView.findViewById(R.id.webview);
+        web.loadUrl("http://users.polytech.unice.fr/~pfz/COURS_SSHOW/finalWord.html");
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+}
